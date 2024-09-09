e026a3b9296a61e1bf4b29bb8470ee78148b3deb
ObliqueHacks <74678940+ObliqueHacks@users.noreply.github.com>
2024-09-09 14:19:51 -0400

    yessirski

--- back-end/demo/src/main/java/com/example/demo/registrationAndLogin/User.java
+++ back-end/demo/src/main/java/com/example/demo/registrationAndLogin/User.java
@@ -1,9 +1,9 @@
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")  // Tells Hibernate to create or map to a table named "users"
@@ -12,11 +12,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(unique = true, nullable = false)
     private String username;
+
+    @Column(nullable = true)
+    private String name;
 
     @Column(nullable = false)
     private String password;
+
+    public User() {}
+
+    public User(String username, String password, String name) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+    }
 
     // Getters and Setters
 }
---
+++ back-end/demo/src/main/java/com/example/demo/registrationAndLogin/UserService.java
@@ -0,0 +1,29 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public User registerUser(UserDTO userDTO) throws Exception {
+        // Check if the username or email already exists
+        Optional<User> existingUserByUsername = userRepository.findByUsername(userDTO.getUsername());
+
+        if (existingUserByUsername.isPresent()) {
+            throw new Exception("Username already exists!");
+        }
+
+        // Encrypt the password
+        String encodedPassword = passwordEncoder.encode(userDTO.getPassword());
+
+        // Create a new User object and save it
+        User newUser = new User(userDTO.getUsername(), encodedPassword, userDTO.getEmail());
+        return userRepository.save(newUser);
+    }
+}
